60a7a83383342071e604d521977e7eff9c1d6aa6
sreenathbs97 <sreenathbs97@gmail.com>
2016-09-28 19:34:13 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var myName = "Sreenath B S";
 
-var app = express();
-app.use(morgan('combined'));
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 50];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue];
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+drawName(myName, letterColors);
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+bounceBubbles();
